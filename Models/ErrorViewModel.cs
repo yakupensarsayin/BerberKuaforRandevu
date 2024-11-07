f25c61f352282e08e6add39d7941eaed2792b984
yakupensarsayin <yakupensarsayin@hotmail.com>
2024-11-07 14:35:48 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BerberKuaforRandevu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
