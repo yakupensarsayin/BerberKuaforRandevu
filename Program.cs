7e464ea2a26a1b827953b3be566666c52a0e4857
Sude Kocaacar <sudekocaacar@gmail.com>
2024-11-09 14:37:45 +0300

    Veritabanını bağla.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using BerberKuaforRandevu.Veritabani;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<KuaforVeritabani>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+        
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Veritabani/KuaforVeritabani.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BerberKuaforRandevu.Veritabani
+{
+    public class KuaforVeritabani(DbContextOptions<KuaforVeritabani> options) : DbContext(options)
+    {
+
+    }
+}
