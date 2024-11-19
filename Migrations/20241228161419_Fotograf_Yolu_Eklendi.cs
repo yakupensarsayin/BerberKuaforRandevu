a535417a37e908aa628400fb12a1f0f12da3ca14
yakupensarsayin <yakupensarsayin@hotmail.com>
2024-11-20 02:56:46 +0300

    Salonlara fotoğraf ekle.

---
+++ Migrations/20241228161419_Fotograf_Yolu_Eklendi.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BerberKuaforRandevu.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fotograf_Yolu_Eklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FotografYolu",
+                table: "Salonlar",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FotografYolu",
+                table: "Salonlar");
+        }
+    }
+}
--- Models/Salon.cs
+++ Models/Salon.cs
@@ -19,6 +19,8 @@ namespace BerberKuaforRandevu.Models
         public SalonTuru SalonTuru { get; set; } = null!;
 
         public List<Kuafor> Kuaforler { get; set; } = [];
+
+        public string? FotografYolu { get; set; }
     }
 
 }
